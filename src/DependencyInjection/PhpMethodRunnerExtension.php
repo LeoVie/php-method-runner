19315109cbea51f8604df2e8897ea43a919a0da8
Leo Viezens <leo.viezens@web.de>
2021-11-02 15:29:07 +0100

    Try adding empty arguments to services config

--- src/DependencyInjection/PhpMethodRunnerExtension.php
+++ src/DependencyInjection/PhpMethodRunnerExtension.php
@@ -8,7 +8,6 @@ use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Routing\Loader\XmlFileLoader;
 
 class PhpMethodRunnerExtension extends Extension
 {
@@ -18,10 +17,10 @@ class PhpMethodRunnerExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $configFile = new FileLocator(__DIR__ . '/../../config/');
+        $configDir = new FileLocator(__DIR__ . '/../../config/');
 
-        $loader = new XmlFileLoader($configFile);
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, $configDir);
+        $loader->load('services.yaml');
 
         $config = $this->processConfiguration(new Configuration(), $configs);
 
